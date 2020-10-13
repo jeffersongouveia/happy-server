3ccec36d1d341bdcfc28739481ead19e0ba23677
Jefferson Gouveia <jeffersongouveia@eucatur.com.br>
2020-10-13 17:56:04 -0400

    🤙 🤙 Error handling

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express'
+import 'express-async-errors'
 
 import '@database/connection'
-import routes from './routes'
+import routes from 'routes'
+import errorHandler from '@errors/handler'
 
 const app = express()
 app.use(express.json())
 app.use(routes)
+app.use(errorHandler)
 
 app.listen(3333, () => {
   console.log('🚀 Server launched')
